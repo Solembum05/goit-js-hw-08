f9fb8cc7f23d12dcc4acb91faa5fc6c0e50691b6
Kysil Ivan <kysilivan1997@gmail.com>
2023-05-01 00:49:36 +0200

    made hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const timeOn = localStorage.getItem('videoplayer-current-time');
+
+const onPlay = function () {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(function (error) {});
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(timeOn)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+  const feedbackObj = {
+    email: e.currentTarget.elements.email.value,
+    message: e.currentTarget.elements.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackObj));
+  loadForm('feedback-form-state');
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(form.email.value);
+  console.log(form.message.value);
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+window.addEventListener('DOMContentLoaded', loadForm);
+function loadForm() {
+  const storage = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (storage !== null) {
+    form.email.value = storage.email;
+    form.message.value = storage.message;
+  }
+}
